6e5a713a79fe42e56b470ea9e4c6521d9d7daed8
Carnegies <casper.12gz@gmail.com>
2023-01-29 12:03:32 +0100

    Keys

--- twitter_spotify_bot/keys.py
+++ twitter_spotify_bot/keys.py
@@ -2,7 +2,7 @@
 # This file is not meant to be used in the main script.
 # Instead, copy this file into keys.py and put your own
 # keys there.
-CONSUMER_KEY = ''
-CONSUMER_SECRET = ''
-ACCESS_KEY = ''
-ACCESS_SECRET = ''
+CONSUMER_KEY = 'OBmITu93sZTakQp2saHFrJHXZ'
+CONSUMER_SECRET = '9Xy36SllWYFbd3w257w59ncUQqb9iQYI6PvhYuD3hJTUobmQ2t'
+ACCESS_KEY = '2391062977-GqDqLWkkNfJY8wXkOIOQYglANLeQiALKGvi78Vp'
+ACCESS_SECRET = 'vFz4NnQ6gU6fDfztttlEJ4G1UuSXzOReRgs7oD7vHIEtK'
